58b2c82e27dd7f355863b6963f0375226039c410
Sandeep Soni <sandeepsoni@deccansoft.net>
2026-02-04 16:28:51 +0530

    Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,13 @@ import TodoList from './components/TodoList'
 import './App.css'
 
 function App() {
-  <h1>This is a demo</h1>
-  return <TodoList />
+  return (
+    <div>
+      <h1>This is a demo</h1>
+      <TodoList />
+      <h2>Footer</h2>
+    </div>
+  )
 }
 
 export default App
